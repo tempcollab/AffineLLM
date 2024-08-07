1d2f4875a89cb4c5521c6030325518427d42b022
tempcollab <86631197+tempcollab@users.noreply.github.com>
2024-08-07 14:33:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ else:
             max_tokens=output_length,
             temperature=temperature,
             # top_p=top_p,
-            top_k=50#int(top_k),
+            topk=50#int(top_k),
             # frequency_penalty=frequency_penalty
         )
 
