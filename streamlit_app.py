9bd71419ef1438712afe76a23b28d66e7e1a0198
tempcollab <86631197+tempcollab@users.noreply.github.com>
2024-08-07 14:39:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ else:
         output_length = st.slider("Output Length", min_value=100, max_value=4096, value=512)
         temperature = st.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7, step=0.05)
         top_p = st.slider("Top-P", min_value=0.0, max_value=1.0, value=0.7, step=0.05)
-        top_k = st.slider("Top-K", min_value=1, max_value=100, value=50, step=1)
-        # frequency_penalty = st.slider("Frequency Penalty", min_value=1.0, max_value=4.0, value=1.0)
+        # top_k = st.slider("Top-K", min_value=1, max_value=100, value=50, step=1)
+        frequency_penalty = st.slider("Frequency Penalty", min_value=1.0, max_value=4.0, value=1.0)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -74,7 +74,7 @@ else:
             temperature=temperature,
             top_p=top_p,
             # topk=50#int(top_k),
-            # frequency_penalty=frequency_penalty
+            frequency_penalty=frequency_penalty
         )
 
         # Stream the response to the chat using `st.write_stream`, then store it in 
