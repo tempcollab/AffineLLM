fe02fbf8d608b69f8a84ef79820c6af79b8a5197
tempcollab <86631197+tempcollab@users.noreply.github.com>
2024-08-07 13:43:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,13 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = st.text_input("API Key", type="password")
 if not openai_api_key:
     st.info("Please add your API key to continue. Request new API keys [here](https://x.com/AffineDeFi).", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key, base_url="http://38.99.105.121:20186")
+    client = OpenAI(api_key=openai_api_key, base_url="http://38.99.105.121:20186/v1")
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -39,7 +39,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
